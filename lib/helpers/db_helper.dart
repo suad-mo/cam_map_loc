bd57f58b31100b9cbc494ed3ba152d7515703745
suad-krvavac <suad.krvavac@gmail.com>
2022-08-15 21:54:28 +0200

    Preparing SQLite...

---
+++ lib/helpers/db_helper.dart
@@ -0,0 +1,24 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+
+class DBHelper {
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final dbPath = await sql.getDatabasesPath();
+    // Otvara ako postoji ili pravi bazu ako je nema places.db
+    final sqlDb = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)',
+        );
+      },
+      version: 1,
+    );
+
+    await sqlDb.insert(
+      table,
+      data,
+      conflictAlgorithm: sql.ConflictAlgorithm.replace,
+    );
+  }
+}
